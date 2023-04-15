1828ae87d928f34c283587937afe546a7a92f87c
Mamat <leenhalabi7@gmail.com>
2023-04-15 14:59:41 +0300

    HW1

--- HW1/scrape_news.sh
+++ HW1/scrape_news.sh
@@ -4,8 +4,8 @@
 	site="https://www.ynetnews.com/category/3082" 
 	data=$(wget --no-check-certificate -O - $site 2>/dev/null)  
 	#search for articles
-	articles=$(echo "$data"  | 
-			grep -oP "https://(www.)?ynetnews.com/article/[0-9a-zA-Z]+" )
+	articles=$(echo "$data"  |
+		grep -oP "https://(www.)?ynetnews.com/article/[0-9a-zA-Z]+" )
 	#counts how many articles are on the site
 	new_articles=$(echo "$articles" | sort | uniq)
 	echo "$(echo "$new_articles" | wc -l)" >> results.csv
@@ -17,14 +17,12 @@
 		num_B=$(echo "$current_site" | grep -o "Bennett" | wc -l)
 		num_P=$(echo "$current_site" | grep -o "Peretz" | wc -l)
 		#zero appearance of the given words case 
-		if [[($num_N -eq 0) && ($num_G -eq 0) && ($num_B -eq 0) && 
-			($num_P -eq 0) ]]; then 
+		if [[($num_N -eq 0) && ($num_G -eq 0) && ($num_B -eq 0) && ($num_P -eq 0) ]]; then 
 			echo "$links, -" >> results.csv  
 			continue
 		fi
 		#prints the results to the results.csv file
-		echo "$links,Netanyahu,$num_N,Gantz,$num_G,Bennett,$num_B,Peretz,$num_P"
-		 >> results.csv
+		echo "$links,Netanyahu,$num_N,Gantz,$num_G,Bennett,$num_B,Peretz,$num_P" >> results.csv
 
 	done 
 
